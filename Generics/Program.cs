c2a5535d01055186d2730724d022aab486ba48dc
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-11 11:00:03 +0530

    [Ajeet] Checking maximum integer value

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace Generics
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("-----  Generics Problems  -----");
+            Console.WriteLine("Please enter three values to find maximum numbers");
+            Console.Write("First no: ");
+            int firstValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Second no: ");
+            int secondValue = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Third no: ");
+            int thirdValue = Convert.ToInt32(Console.ReadLine());
+            Maximum maximum = new Maximum();
+            int output = Maximum.IntegerNo(firstValue, secondValue, thirdValue);
+            Console.WriteLine("{0} is the maximum number\n at position no {1}", output, value);
         }
     }
 }
