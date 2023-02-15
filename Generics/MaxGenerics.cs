f51c5de42796b37de5c3bebcf1402b84746d8221
ajeet1010 <ajeetpal101999@gmail.com>
2023-02-15 16:59:09 +0530

    [Ajeet] Find mximum array using instance generics.

---
+++ Generics/MaxGenerics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class MaxGenerics<T>
+    {
+        // Method to find maximum by taking more than three parameters.
+
+        public T[] array;
+        public MaxGenerics(T[] array)
+        {
+            this.array = array;
+        }
+        public void TestMaximum()
+        {
+            FindMaxValues(array);
+        }
+        public static void FindMaxValues(T[] array)
+        {
+            int length = array.Length;
+            Console.WriteLine("Length of array  is:  " + length);
+            //sorting in ascending order
+            Array.Sort(array);
+            foreach (T item in array)
+            {
+                Console.Write(item + "      ");
+            }
+            Console.WriteLine("\nMaximum Value is: " + array[length - 1]);
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -7,10 +7,25 @@
             Console.WriteLine("-----  Generics Problems  -----");
             int[] intArray = { 10, 20, 30, 40, 50, 80 };
             float[] floatArray = { 54.70f, 58.00f, 63.47f, 58.98f, 98.36f, 25.3f };
+            double[] doubleArray = { 12.7, 56.8, 89.2, 19.4, 66.5, 56.8 };
             string[] stringArray = { "WHAT", "ARE", "YOU", "DOING" };
-            MaxMethod.Maximum(intArray);
-            MaxMethod.Maximum(floatArray);
-            MaxMethod.Maximum(stringArray);
+
+            Console.WriteLine("\n\n***** Integer Array  *****");
+            MaxGenerics<int> integerobj = new MaxGenerics<int>(intArray);
+            integerobj.TestMaximum();
+
+            Console.WriteLine("\n\n***** Double Array  *****");
+            MaxGenerics<double> doubleobj = new MaxGenerics<double>(doubleArray);
+            doubleobj.TestMaximum();
+
+            Console.WriteLine("\n\n***** String Array  *****");
+            MaxGenerics<string> stringobj = new MaxGenerics<string>(stringArray);
+            stringobj.TestMaximum();
+
+            Console.WriteLine("\n\n***** Float Array  *****");
+            MaxGenerics<float> floatobj = new MaxGenerics<float>(floatArray);
+            floatobj.TestMaximum();
+
         }
     }
 }
